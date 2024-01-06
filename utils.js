3cf9ff3a4bbd446944041c2021d3c297d7b0d090
Erkin Öztürk <erkinozturk@outlook.com.tr>
2024-01-06 16:40:56 +0300

    Simulating traffic

--- utils.js
+++ utils.js
@@ -4,8 +4,8 @@ function lerp(A, B, t) {
 
 function getIntersection(A, B, C, D) {
     const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
-    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.x - B.y);
-    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (A.y - A.y);
+    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
     if (bottom != 0) {
         const t = tTop / bottom;
